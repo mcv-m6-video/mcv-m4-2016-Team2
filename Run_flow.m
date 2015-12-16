d2764789aac173f78b42f41ad3d76ff8ac874e68
Jordina Torrents Barrena <utenajtb@gmail.com>
2015-12-16 22:54:46 +0100

    Update Run_flow.m
    Main to run the task 7

--- Run_flow.m
+++ Run_flow.m
@@ -11,7 +11,18 @@ MSEResults = MSEImages(testImages, groundTruth)
 
 PEPNResults = PEPN(testImages, groundTruth)
 
-
 %plotResults('TestB', TestB_evaluationSequence, TestB_evaluationFrame, cfg)
 % [testABoundingBox] = ExtractBoundingBox(testAImages);
 
+
+%% Task 7
+% Plot the optical flow
+
+for index = 1:length(testImages)
+    
+    % Read real image
+    [real_img, map] = imread([config.images_flow '/' result_img{index}(8:end)]);
+    
+    % Plot Optical Flow results
+    plotOpticalFlow(real_img, map, result_img{index});
+end
