65093caf69c277c5181c7f35acfd75a49118eff5
monica <mnc537@gmail.com>
2015-12-23 16:43:59 +0100

    Optional 6: GMM gets poor results

---
+++ Week2/CreateVideosDatabase.m
@@ -0,0 +1,28 @@
+function CreateVideosDatabase()
+highwayFrames = [1050:1350];
+fallFrames =    [1460:1560];
+trafficFrames = [950:1050];
+
+rootDir = '../../Data/';
+createVideo(rootDir, 'Highway', highwayFrames);
+createVideo(rootDir, 'fall', fallFrames);
+createVideo(rootDir, 'traffic', trafficFrames);
+
+function createVideo (workingDir, sequence, frames)
+    workingDir = fullfile(rootDir, sequence, 'input/');
+    images = LoadImages(workingDir, frames, 'in', 'jpg');
+
+    % create new video object
+    outputVideo = VideoWriter(fullfile(rootDir, sequence, 'video.avi'));
+    open(outputVideo);
+
+    for ii = 1:length(images)
+       img = images{ii};
+       writeVideo(outputVideo,img)
+    end
+
+    close(outputVideo)
+
+end
+
+end
