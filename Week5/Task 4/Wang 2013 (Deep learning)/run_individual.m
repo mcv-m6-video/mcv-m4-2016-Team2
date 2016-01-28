3f6b6216a5de0ac81676dc12aac93c6f59da1863
Martin Matilla <matilla.martin@gmail.com>
2016-01-28 21:53:48 +0100

    Wang working

--- Week5/Task 4/Wang 2013 (Deep learning)/run_individual.m
+++ Week5/Task 4/Wang 2013 (Deep learning)/run_individual.m
@@ -36,64 +36,32 @@
 %    affsig(5) = aspect ratio
 %    affsig(6) = skew angle
 clear all
-dataPath = 'D:\Dropbox\dropbox\Tracking\data\';
-% dataPath = 'F:\dropbox\Tracking\data\';
-title = 'woman';
+cfg = Config();
 
-switch (title)
-case 'davidin';  p = [158 106 62 78 0];
-    opt = struct('numsample',1000, 'affsig',[4, 4,.005,.00,.001,.00]);
-case 'trellis';  p = [200 100 45 49 0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.00, 0.00, 0.00, 0.0]);
-case 'car4';  p = [123 94 107 87 0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.02,.0,.001,.00]);
-case 'car11';  p = [88 139 30 25 0];
-    opt = struct('numsample',1000,'affsig',[4,4,.005,.0,.001,.00]);
-case 'animal'; p = [350 40 100 70 0];
-    opt = struct('numsample',1000,'affsig',[12, 12,.005, .0, .001, 0.00]);
-case 'shaking';  p = [250 170 60 70 0];%
-    opt = struct('numsample',1000, 'affsig',[4,4,.005,.00,.001,.00]);
-case 'singer1';  p = [100 200 100 300 0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.01,.00,.001,.0000]);
-case 'bolt';  p = [292 107 25 60 0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.005,.000,.001,.000]);
-case 'woman';  p = [222 165 35 95 0.0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.005,.000,.001,.000]);               
-case 'bird2';  p = [116 254 68 72 0.0];
-    opt = struct('numsample',1000, 'affsig',[4,4,.005,.000,.001,.000]); 
-case 'surfer';  p = [286 152 32 35 0.0];
-    opt = struct('numsample',1000,'affsig',[8,8,.01,.000,.001,.000]);     
-otherwise;  error(['unknown title ' title]);
-end
+highway = LoadHighway(cfg);
+
+title = 'highway'
+
+p = [220 10 23 23 0.0];
+opt = struct('numsample',1000,'affsig',[8,8,.01,.000,.001,.000]);
 
 % The number of previous frames used as positive samples.
 opt.maxbasis = 10;
 opt.updateThres = 0.8;
 % Indicate whether to use GPU in computation.
-global useGpu;
-useGpu = true;
+% global useGpu;
+% useGpu = true;
 opt.condenssig = 0.01;
 opt.tmplsize = [32, 32];
 opt.normalWidth = 320;
 opt.normalHeight = 240;
 seq.init_rect = [p(1) - p(3) / 2, p(2) - p(4) / 2, p(3), p(4), p(5)];
 
-% Load data
-disp('Loading data...');
-fullPath = [dataPath, title, '\'];
-d = dir([fullPath, '*.jpg']);
-if size(d, 1) == 0
-    d = dir([fullPath, '*.png']);
-end
-if size(d, 1) == 0
-    d = dir([fullPath, '*.bmp']);
-end
-im = imread([fullPath, d(1).name]);
-data = zeros(size(im, 1), size(im, 2), size(d, 1));
-seq.s_frames = cell(size(d, 1), 1);
-for i = 1 : size(d, 1)
-    seq.s_frames{i} = [fullPath, d(i).name];
-end
+completePath  = arrayfun(@(c)sprintf('%s%s%06d.%s',cfg.highway.inputPath, 'in' , c, 'jpg') , cfg.highway.testFrames,...
+                'UniformOutput', false);
+            
+seq.s_frames = completePath';
+
 seq.opt = opt;
-results = run_DLT(seq, '', false);
+results = run_DLT(seq, 'results', true);
 save([title '_res'], 'results');
